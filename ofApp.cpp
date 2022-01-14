9a7f7f034536146632b9c1c06232ea658b707178
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2022-01-14 18:32:37 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,98 @@
+﻿#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(30);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(255);
+	ofSetLineWidth(3);
+	ofEnableDepthTest();
+
+	this->noise_seed = glm::vec3(ofRandom(1000), ofRandom(1000), ofRandom(1000));
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->cam.begin();
+	ofRotateY(ofGetFrameNum());
+
+	auto size = 40;
+	auto noise_seed = ofRandom(1000);
+	for (int x = ofGetWidth() * -0.5 + size; x < ofGetWidth() * 0.5; x += size * 2) {
+
+		for (int y = ofGetWidth() * -0.5 + size; y < ofGetWidth() * 0.5; y += size * 2) {
+
+			for (int z = ofGetWidth() * -0.5 + size; z < ofGetWidth() * 0.5; z += size * 2) {
+
+				auto location = glm::vec3(x, y, z);
+				auto target = glm::vec3(x, y + 1, z);
+
+				this->draw_arrow(location, target, size, ofColor(255), ofColor(0));
+			}
+		}
+	}
+
+	this->cam.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw_arrow(glm::vec3 location, glm::vec3 target, float size, ofColor fill_color, ofColor no_fill_color) {
+
+	ofPushMatrix();
+	ofTranslate(location);
+
+	ofRotateZ(ofMap(ofNoise(this->noise_seed.z + ofGetFrameNum() * 0.01, location.x * 0.0003, location.y * 0.0003, location.z * 0.0003), 0, 1, -540, 540));
+	ofRotateY(ofMap(ofNoise(this->noise_seed.y + ofGetFrameNum() * 0.01, location.x * 0.0003, location.y * 0.0003, location.z * 0.0003), 0, 1, -540, 540));
+	ofRotateX(ofMap(ofNoise(this->noise_seed.x + ofGetFrameNum() * 0.01, location.x * 0.0003, location.y * 0.0003, location.z * 0.0003), 0, 1, -540, 540));
+
+	auto angle = std::atan2(target.y - location.y, target.x - location.x);
+
+	ofSetColor(fill_color);
+	ofFill();
+	ofBeginShape();
+	ofVertex(glm::vec2(size * cos(angle), size * sin(angle)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)));
+	ofEndShape();
+
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25);
+	ofEndShape();
+
+	ofSetColor(no_fill_color);
+	ofNoFill();
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)));
+	ofVertex(glm::vec2(size * cos(angle), size * sin(angle)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)));
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25);
+	ofEndShape();
+
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25);
+	ofEndShape();
+
+	ofPopMatrix();
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
